f09837811471ac85f1b98f72dc61617ccddbd02d
BrainX Shopify <brainxshopify@BrainXs-MacBook-Pro.local>
2024-10-10 13:16:41 +0500

    css changes and cart issue resolved in collections

--- assets/prevent-default-add-to-cart.js
+++
@@ -1,8 +0,0 @@
-  document.addEventListener('DOMContentLoaded', function() {
-    const addToCartButton = document.querySelector('.quick-add__submit');
-
-    addToCartButton.addEventListener('click', function(event) {
-        event.preventDefault();
-        console.log('Add to Cart button clicked, but form submission prevented');
-    });
-});
